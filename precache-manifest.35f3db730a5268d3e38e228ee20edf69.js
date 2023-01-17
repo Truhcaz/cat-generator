f63f1bb6d3d0248b9d459ee16df87e9f4ddd214f
Truhcaz <mateo.brc1@gmail.com>
2023-01-17 17:26:09 +0100

    Updates

---
+++ precache-manifest.35f3db730a5268d3e38e228ee20edf69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c69a0d69790e37265d4e",
+    "url": "/cat-generator/static/css/main.97e77a16.chunk.css"
+  },
+  {
+    "revision": "c69a0d69790e37265d4e",
+    "url": "/cat-generator/static/js/main.c69a0d69.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/cat-generator/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "ebd5b217692bce4a6d85",
+    "url": "/cat-generator/static/js/2.ebd5b217.chunk.js"
+  },
+  {
+    "revision": "7584d917db1bb8f44b3d",
+    "url": "/cat-generator/static/js/runtime~main.7584d917.js"
+  },
+  {
+    "revision": "23fdefde75e8e7258db2f2decbb3c859",
+    "url": "/cat-generator/static/media/orientalcatslight-2ozb8-webfont.23fdefde.woff2"
+  },
+  {
+    "revision": "8bbfb704a5900a18fd3122378fbb487d",
+    "url": "/cat-generator/static/media/orientalcatslight-2ozb8-webfont.8bbfb704.woff"
+  },
+  {
+    "revision": "4a264680e02326f25556f82d32dfade2",
+    "url": "/cat-generator/index.html"
+  }
+];
